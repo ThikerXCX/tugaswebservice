bfd5c67d5ee27f1c964454ad0fdbe4bba71bfd59
ThikerXCX <rifkimuhammad967@gmail.com>
2022-01-07 18:24:53 +0700

    push aja

--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -33,7 +33,7 @@
                                     <td>{{ $loop->iteration }}</td>
                                     <td>{{ $i->name }}</td>
                                     <td>
-                                        <button class="btn btn-warning edit" id="edit" value="{{ $i->id }}"><i class="far fa-edit"></i></button>
+                                        <button class="btn btn-warning edit" data-bs-toggle="modal" data-bs-target="#editmodal" id="edit" value="{{ $i->id }}"><i class="far fa-edit"></i></button>
                                         <form action="#" method="post" class="d-inline">
                                             @csrf
                                             @method('delete')
@@ -72,13 +72,36 @@
             </div>
         </div>
     </div>
+    <div class="modal fade" id="editmsodal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Tambah Data</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <form action="" method="POST">
+                    @csrf
+                    <div class="mb-3">
+                        <label for="name" class="form-label">Nama Kategori</label>
+                        <input type="text" class="form-control" id="name" name="name">
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="submit" class="btn btn-primary">Simpan</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
     @slot('js')
         <script>
             $(document).ready(function() {
                 $('#example').DataTable();
             } );
             $('.edit').on('click',function(e){
-                const x = document.getElementsByClassName("edit").value;
+                var x = $(this).val();
                 $.ajax({
                     method : 'get',
                     url : "/categori/" + x,
